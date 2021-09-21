edd9a63b390a6e7d11c2a359a80b2ec28adbe06c
Dan Heidinga <heidinga@redhat.com>
2021-09-21 14:48:41 -0400

    More bcReachabilityAnalzyer changes
    Now walking the blocks 1-by-1 and recording the classes.
    
    TODO:
    * Remove the printfs
    * expose the _discovered_classes to the user
    * expose the enqueuedMethods to the user
    * track the invoking bytecode for enqueued methods

--- src/hotspot/share/ci/bcReachabilityAnalyzer.hpp
+++ src/hotspot/share/ci/bcReachabilityAnalyzer.hpp
@@ -2,12 +2,34 @@
 #define SHARE_CI_BCREACHABILITYANALYZER_HPP
 
 #include "ci/ciMethod.hpp"
+#include "ci/ciMethodBlocks.hpp"
+#include "ci/ciType.hpp"
+#include "utilities/growableArray.hpp"
 
 class BCReachabilityAnalyzer : public ResourceObj {
+
+private:
+  Arena *_arena;        // ciEnv arena
+  ciMethod *_method;
+  ciMethodBlocks *_methodBlocks;
+
+  BCReachabilityAnalyzer *_parent;
+
+  GrowableArray<ciType *> _discovered_klasses;
+
+private:
+  void iterate_blocks();
+  void iterate_one_block(ciBlock *blk, GrowableArray<ciBlock *> &successors);
+
+  void recordReferencedType(ciType *type);
+  void enqueueMethod(ciMethod *method);
+
 
  public:
   BCReachabilityAnalyzer(ciMethod* method, BCReachabilityAnalyzer* parent = NULL);
 
+  BCReachabilityAnalyzer* parent() { return _parent; }
+  ciMethod* method() const { return _method; }
 };
 
 #endif //SHARE_CI_BCREACHABILITYANALYZER_HPP
