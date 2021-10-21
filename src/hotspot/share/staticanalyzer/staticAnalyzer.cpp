ffa7eb5d1c47133ee815c82a820fd64cf87dfca0
Dan Heidinga <heidinga@redhat.com>
2021-10-21 16:04:51 -0400

    Push results of bcra back into Java
    Use JavaCalls to call the StaticAnalyzer java class and enqueue the found methods
    Similar treatment for the discovered classes
    
    Doesn't fully work yet - still dealing with assert failures due to
    round tripping abstract / native methods.

--- src/hotspot/share/staticanalyzer/staticAnalyzer.cpp
+++ src/hotspot/share/staticanalyzer/staticAnalyzer.cpp
@@ -23,9 +23,11 @@
  */
 
 #include "ci/bcReachabilityAnalyzer.hpp"
+#include "classfile/symbolTable.hpp"
 #include "compiler/compilerDirectives.hpp"
 #include "memory/oopFactory.hpp"
 #include "runtime/handles.inline.hpp"
+#include "runtime/interfaceSupport.inline.hpp"
 #include "runtime/javaCalls.hpp"
 #include "staticanalyzer/saJavaClasses.hpp"
 #include "staticanalyzer/staticAnalyzer.hpp"
@@ -37,27 +39,63 @@ void StaticAnalyzer::initialize() {
 
 void StaticAnalyzer::compile_method(ciEnv* env, ciMethod* target, int entry_bci, bool install_code, DirectiveSet* directive) {
   JavaThread* THREAD = JavaThread::current();
+  printf("StaticAnalyzer::compile_method>>>\n");
   // do reachability analysis
   BCReachabilityAnalyzer *bcra = target->get_bcra();
-
+printf("# bcra computed\n");
   // TODO: load classes discovered by bcra
-
-  //make upcall to add callees in the set of methods to be analyzed
-  GrowableArray<ciMethod*> callees = bcra->get_callees();
-  int numCallees = callees.length();
-  if (numCallees > 0) {
-    typeArrayOop calleeHandles = oopFactory::new_longArray(numCallees, CHECK);
-    int i = 0;
-    for (GrowableArrayIterator<ciMethod*> it = callees.begin(); it != callees.end(); ++it) {
-      ciMethod *method = *it;
-      jlong methodId = (jlong)method->get_Method();
-      calleeHandles->long_at_put(i, methodId);
-      i += 1;
+  {
+    ThreadInVMfromUnknown tiv;
+    ResetNoHandleMark rnhm;
+    Klass *accessing_klass = target->get_Method()->constants()->pool_holder(); /// TODO
+    Handle class_loader = Handle(THREAD, accessing_klass->class_loader());
+    Handle protection_domain  = Handle(THREAD, accessing_klass->protection_domain());
+    
+    GrowableArray<ciType*> classes = bcra->get_classes();
+    for (GrowableArrayIterator<ciType*> it = classes.begin(); it != classes.end(); ++it) {
+      ciType *c = *it;
+      ciKlass *k = c->as_klass();
+      Klass *klazz = nullptr;
+      if (c->is_loaded() && false) {
+        // get_Klass() is protected, can't use here.  Force the lookup
+        //klazz = k->get_Klass();
+      } else {
+        Symbol *name_symbol = SymbolTable::new_symbol(k->name()->as_utf8(), k->name()->utf8_length());
+        klazz = SystemDictionary::resolve_or_null(name_symbol, class_loader, protection_domain, THREAD);
+      }
+      if (klazz != nullptr) {
+        JavaValue result(T_VOID);
+        JavaCallArguments args;
+        args.push_oop(Handle(THREAD, klazz->java_mirror()));
+        JavaCalls::call_static(&result, jdk_internal_staticanalysis_StaticAnalyzer::klass(), vmSymbols::addDiscoveredClass_name(), vmSymbols::class_void_signature(), &args, THREAD);
+      }
     }
-    JavaValue result(T_OBJECT);
-    JavaCallArguments args;
-    args.push_oop(Handle(THREAD, calleeHandles));
-    JavaCalls::call_static(&result, jdk_internal_staticanalysis_StaticAnalyzer::klass(), vmSymbols::addToQueue_name(), vmSymbols::addToQueue_name_signature(), &args, THREAD);
+  
+
+    //make upcall to add callees in the set of methods to be analyzed
+    GrowableArray<ciMethod*> callees = bcra->get_callees();
+    int numCallees = callees.length();
+    if (numCallees > 0) {
+      //ResetNoHandleMark rnhm;
+      typeArrayOop calleeHandles = oopFactory::new_longArray(numCallees, CHECK);
+      int i = 0;
+      for (GrowableArrayIterator<ciMethod*> it = callees.begin(); it != callees.end(); ++it) {
+        ciMethod *method = *it;
+        if (method->is_loaded()) {
+          jlong methodId = (jlong)method->get_Method();
+          calleeHandles->long_at_put(i, methodId);
+          i += 1;
+        } else {
+          printf("# TODO deal with unloaded methods\n");
+        }
+      }
+      JavaValue result(T_VOID);
+      JavaCallArguments args;
+      args.push_oop(Handle(THREAD, calleeHandles));
+      printf("# calling into Java\n");
+      JavaCalls::call_static(&result, jdk_internal_staticanalysis_StaticAnalyzer::klass(), vmSymbols::addToQueue_name(), vmSymbols::addToQueueA_signature(), &args, THREAD);
+    }
   }
+  printf("<<< StaticAnalyzer::compile_method\n");
 }
 
